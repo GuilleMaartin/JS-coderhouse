191164de057df769ff3d6b4a9cda5c83de976f80
Guillermo Rossi <guillemaartin.gr@gmail.com>
2022-06-09 15:07:04 -0300

    calculadora js

--- java.js
+++ java.js
@@ -1,71 +1,14 @@
-//
+  const articulo = document.querySelector("#contenido");
+  const button = document.querySelector("#leer-mas");
+            
+            function leerMas(){
+                if (articulo.className === "abierto"){
+                  articulo.className = "";
+                  button.textContent = "Mostrar mas"
+                } else {
+                  articulo.className = "abierto";
+                  button.textContent = "Mostrar menos"
+                }
+            } 
 
-let number = prompt ("inserte numero")
-
-if (number <= 10){
-  console.log ("primera etapa");
-  } 
-else if (number > 11 && number <= 34){
-  console.log ("segunda etapa");
-  } 
-else if  (number >= 35 && number <=67) {
-    console.log ("its ok");
-} 
-else if (number >=68 && number <= 100){
-  console.log ("deberias chequear tu edad");
-}
-//
-//                    //
-//
-//let nombre = prompt ("ingrese su nombre")
-//
-//let apellido = prompt ("ingrese su apellido")
-//
-//if ((nombre != "") && (apellido != "")){
-//  alert ("Nombre: " +  nombre + 
-//   " Apellido : " + apellido )
-//} 
-//else ("Error: Ingresar nombre y apellido")
-//
-//                    //
-
-                    
-let entrada = prompt ("¿Que desea llevar?");
-
-
-
-while (entrada != "ESC"){
-  switch (entrada){
-    case "coca": 
-        alert ("nos queda pepsi");
-        break;
-    case "sprite":
-        alert ("nos queda 7up");
-        break;
-    case "fanta":
-        alert ("nos queda mirinda");
-        break;
-    case "shueps":
-        alert ("nos queda agua tonica linea pepsi");
-        break;
-    default:
-        alert ("Siguiente");
-        break; 
-  }
-  entrada = prompt ("Que desea llevar");
-  
-} 
-
-
-let producto = prompt ("Que desea llevar?")
-let contador = 0
-
-while (producto != "camara") {
-    contador++
-    alert ("Necesita algo mas?")
-    producto = prompt ("Que otra cosa, decime?")
-} 
-alert (" No nos queda camaras en este momento")
-
-
-
+            leerMas()
--- tomi.js
+++ tomi.js
@@ -1,59 +1,85 @@
-let envio = prompt ("En que estado se encuentra?")
+//let envio = prompt ("En que estado se encuentra?")
+//
+////cliente fuera de domicilio = reintentar
+////siniestrado = reclamar 
+////entregado = ok
+////en destino = ok
+//
+//switch (envio){
+//    case "entregado" || "en destino":
+//        alert ("ok")
+//        break;
+//    case "siniestrado":
+//        alert ("reclamar")
+//        break;
+//    case "cliente fuera de domicilio":
+//        alert ("reintentar")
+//        break;
+//    default:
+//        alert ("no existe este estado");
+//        prompt ("¿desea repetirlo?:")
+//        break;
+//}
+//function calculadora(parametro1, parametro2, operacion){
+//    switch(operacion){
+//        case "+":
+//            return parametro1 + parametro2;
+//            break;
+//        case "-":
+//            return parametro1 - parametro2;
+//            break;
+//        case "/":
+//            return parametro1 / parametro2;
+//            break;
+//        case "*":
+//            return parametro1 * parametro2;
+//            break;
+//        default:
+//            return 0;
+//            break;
+//    }
+//}
+//calculadora()
 
-//cliente fuera de domicilio = reintentar
-//siniestrado = reclamar 
-//entregado = ok
-//en destino = ok
 
-switch (envio){
-    case "entregado" || "en destino":
-        alert ("ok")
-        break;
-    case "siniestrado":
-        alert ("reclamar")
-        break;
-    case "cliente fuera de domicilio":
-        alert ("reintentar")
-        break;
-    default:
-        alert ("no existe este estado");
-        prompt ("¿desea repetirlo?:")
-        break;
+
+function suma (){
+    let dato = 0;
+    dato = 8 + 9;
+    alert(dato)
 }
+suma()
+
+function menos(){
+    let resta = 0;
+    resta = 4-3;
+    alert(resta)
+}
+menos()
+
+
 function calculadora(parametro1, parametro2, operacion){
-    switch(operacion){
+
+    switch (operacion){
         case "+":
             return parametro1 + parametro2;
             break;
         case "-":
-            return parametro1 - parametro2;
-            break;
+                return parametro1 - parametro2;
+                break;
         case "/":
             return parametro1 / parametro2;
             break;
         case "*":
             return parametro1 * parametro2;
             break;
-        default:
+        default :
             return 0;
-            break;
-    }
+            break;   
 }
-calculadora()
 
-function pedido(){
-    switch (pedido){
-        case "recibido":
-            return prompt("its ok");
-            break;
-        case "entregado en el domicilio":
-            return prompt("its ok");
-            break;
-        case "extraviado":
-            return prompt ("notificado");
-            break;
-        default:
-            return 0;
-            break;
-    }
 }
+
+alert (calculadora());
+
+
